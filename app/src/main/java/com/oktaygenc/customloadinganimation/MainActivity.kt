d98fdffa4dcc8198b11fcc50459304b728360f5f
Oktay Genc <gncoktay3866@gmail.com>
2024-11-07 01:09:34 +0300

    feat: Integrate CustomLoadingDialog with simulated long task in MainActivity

--- app/src/main/java/com/oktaygenc/customloadinganimation/MainActivity.kt
+++ app/src/main/java/com/oktaygenc/customloadinganimation/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.oktaygenc.customloadinganimation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var loadingDialog: CustomLoadingDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        loadingDialog = CustomLoadingDialog(this)
+
+        val button: Button = findViewById(R.id.startButton)
+        button.setOnClickListener {
+            loadingDialog.show("Veri Yükleniyor...")
+            simulateLongTask()
         }
+    }
+
+    private fun simulateLongTask() {
+        Handler().postDelayed({
+            loadingDialog.dismiss()
+            Toast.makeText(this, "İşlem Tamamlandı", Toast.LENGTH_SHORT).show()
+        }, 3000)
     }
 }
